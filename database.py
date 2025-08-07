03ba7eddd66ab82c682cae136f82a975fe2d934c
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2025-08-07 19:54:44 +0200

    chore: :zap: Update database files - Improve code structure!

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from models import Base
+from settings import AppSettings
 
-# Corrected database URL
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://fastapi:PASSWORD@localhost/backend"
+# Get settings
+settings = AppSettings()
 
+# Use the database URL from settings
+SQLALCHEMY_DATABASE_URL = settings.constructed_database_url
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 async def init_db() -> None:
@@ -12,3 +15,27 @@ async def init_db() -> None:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     print("Database initialized successfully.")
+
+from typing import Annotated
+
+from database import engine
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+async_session = async_sessionmaker(
+    bind=engine, class_=AsyncSession, autocommit=False, autoflush=False
+)
+
+
+async def get_db_session():
+    try:
+        async with async_session() as session:
+            yield session
+    except:
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
+
+
+DBSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
