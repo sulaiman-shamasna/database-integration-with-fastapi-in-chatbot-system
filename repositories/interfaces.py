dba9efaad3af63a44f20da6d7653d05a33e0ac22
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2025-08-07 20:07:11 +0200

    feat(db): :card_file_box: add abstract repository interface for async CRUD operations

---
+++ repositories/interfaces.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+class Repository(ABC):
+    @abstractmethod
+    async def list(self) -> list[Any]:
+        pass
+
+    @abstractmethod
+    async def get(self, uid: int) -> Any:
+        pass
+
+    @abstractmethod
+    async def create(self, record: Any) -> Any:
+        pass
+
+    @abstractmethod
+    async def update(self, uid: int, record: Any) -> Any:
+        pass
+
+    @abstractmethod
+    async def delete(self, uid: int) -> None:
+        pass
